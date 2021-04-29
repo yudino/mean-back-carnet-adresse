771a69598b28ef099a18e7c787ab785cf3127e78
yudino <yohane.udino@gmail.com>
2021-04-29 22:20:03 +0200

    auth marche avec MongoDB

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path');
 const schtroumpfRoutes = require('./routes/schtroumpf');
 const userRoutes = require('./routes/user');
 
-mongoose.connect('mongodb+srv://yudino:yudinoAdmin@cluster0.bmpho.mongodb.net/myFirstDatabase?retryWrites=true&w=majorityy',
+mongoose.connect('mongodb+srv://yudino:yudinoAdmin@cluster0.bmpho.mongodb.net/smurfFriends?retryWrites=true&w=majority',
     { useNewUrlParser: true,
         useUnifiedTopology: true })
     .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 app.use('/images', express.static(path.join(__dirname, 'images')));
-app.use('/carnet', schtroumpfRoutes);
-app.use('/auth', userRoutes);
+app.use('/api/carnet', schtroumpfRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
